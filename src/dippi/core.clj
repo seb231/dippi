0ce571ef499e8a35cd839f0267549856944a1fdb
Seb Bailey <sebastian@mastodonc.com>
2017-08-28 22:33:53 +0100

    WIP: filter fn

--- src/dippi/core.clj
+++ src/dippi/core.clj
@@ -28,6 +28,12 @@
   [database query]
   (search-nhm-api database "&q=" query))
 
+(defn filter-nhm-api
+  "Expects a name of a database (see http://data.nhm.ac.uk/dataset?author=Natural+History+Museum)
+   and a filter field and search string i.e \"Archaeopteryx\""
+  [database field query]
+  (search-nhm-api database "&filters=" "{field:query}"))
+
 (def collection-specimens "resource_id=05ff2255-c38a-40c9-b657-4ccb55ab2feb")
 
 (def artefacts "resource_id=ec61d82a-748d-4b53-8e99-3e708e76bc4d" )
