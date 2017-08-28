c74ba82070da6fde6d233385763f1ebfa3154578
Seb Bailey <sebastian@mastodonc.com>
2017-08-28 16:35:42 +0100

    Search API fn

--- src/dippi/core.clj
+++ src/dippi/core.clj
@@ -9,3 +9,20 @@
     (if (= 200 (:status response))
       (get-in response [:body :result])
       nil)))
+
+(defn search-nhm-api
+  "Takes a database, search type and string to search by.
+  Possible databases are
+
+  - collection-specimens
+
+  - artefacts
+
+  "
+  [database search-type search-by]
+  (slurp-json (str "http://data.nhm.ac.uk/api/action/datastore_search?" database search-type search-by)))
+
+(def collection-specimens "resource_id=05ff2255-c38a-40c9-b657-4ccb55ab2feb")
+
+(def artefacts "resource_id=ec61d82a-748d-4b53-8e99-3e708e76bc4d" )
+
